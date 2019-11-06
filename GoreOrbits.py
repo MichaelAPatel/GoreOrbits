09357f23496c3c57594c8fc63693af6dcac34194
michael.patel <michael.patel@LIGO.org>
2019-11-06 11:42:08 -0500

    Playing with matplotlib...

--- GoreOrbits.py
+++ GoreOrbits.py
@@ -2,3 +2,18 @@
 # This is a small, failing, application to simulate the orbits of charged particles
 # around a spinning solid sphere of charge.
 
+import matplotlib.pyplot as plt
+import numpy
+
+omega = 0
+q1 = 0
+q2 = 0
+phidoti = 0
+
+for i in range(0,100000):
+    #Do some calculations that we dont know how to do...
+    q1 += 1
+
+fig = plt.figure()  # an empty figure with no axes
+fig.suptitle('Eventually we will have a cool graph here!')
+plt.show()
